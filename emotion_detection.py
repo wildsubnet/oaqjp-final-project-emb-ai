f905698cf793b81898dcb82096eab54e1e8ecf12
Matthew Tighe <matthew.e.tighe@gmail.com>
2024-09-30 16:36:23 -0400

    Updated to format output as requested

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,5 @@
 import requests  
-
+import json
 
 def emotion_detector(text_to_analyze):  
     # Define a function named emotion_detector that takes a string input (text_to_analyze)
@@ -7,4 +7,16 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text
+    fmt_response = json.loads(response.text)
+    emotions=fmt_response['emotionPredictions'][0]['emotion']
+    dom_emote = ''
+    dom_score = 0
+    print("{")
+    for emote,score in emotions.items():
+        print(f"'{emote}': {score},")
+        if score > dom_score:
+            dom_emote = emote
+            dom_score = score
+    emotions['dominant_emotion']=dom_emote
+    print(f"'dominant_emotion': '{dom_emote}'")
+    print("}")
